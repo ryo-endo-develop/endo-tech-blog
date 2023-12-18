944695a0b18b62462a73e4e9783f191ca1e0b964
ryo-endo-develop <r3endo.dev@gmail.com>
2023-12-18 18:34:51 +0900

    ヘッダーの修正

---
+++ frontend/src/app/ui/mame-logo.tsx
@@ -0,0 +1,9 @@
+import { lusitana } from "@/app/ui/fonts";
+
+export default function MameLogo() {
+    return (
+        <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
+            <p className="text-[44px]">Mame</p>
+        </div>
+    );
+}
